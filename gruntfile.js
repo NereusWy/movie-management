7d85721f4bc9c29b1c93519ee9866434ffaed16d
yiwang <18842613507@163.com>
2018-05-23 17:01:38 +0800

    add grunt

---
+++ gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		watch:{
+			jade:{
+				files:['views/**'],
+				options:{
+					livereload:true
+				}
+			},
+			js:{
+				files:['public/js/**','models/**/*.js','schemas/**/*.js',],
+				//tasks:['jshint'],
+				options:{
+					livereload:true
+				}
+			}
+		},
+
+		nodemon:{
+			dev:{
+				options:{
+					file:'app.js',
+					args:[],
+					ignoredFiles:['README.md','node_modules/**','.DS_Store'],
+					watchedExtensions:['js'],
+					watchedFolders:['app','config'],
+					debug:true,
+					delayTime:1,
+					env:{
+						PORT:3000
+					},
+					cwd:__dirname	
+				}
+			}
+		},
+
+		concurrent:{
+			tasks:['nodemon','watch'],
+			options:{
+				logConcurrentOutput:true
+			}
+		},
+	})
+
+	grunt.loadNpmTasks('grunt-contrib-watch') 	//文件添加、修改、删除会重新执行添加的任务
+	grunt.loadNpmTasks('grunt-nodemon')			//实时监听app.js,改动后会自动重启app.js
+	grunt.loadNpmTasks('grunt-concurrent')		//"慢任务-sass/less/coffee的编辑"优化构建时间
+
+	grunt.option('force',true)		//防止因为grunt的错误而终止grunt的编译
+	grunt.registerTask('default',['concurrent'])
+}
